16cc72efa973185491ca2a5d13400449d4ae80a7
Scuderiasy <40252740+Scuderiasy@users.noreply.github.com>
2019-09-24 07:12:25 +0700

    Add files via upload

---
+++ AI Insertion Sort/AI Insertion Sort/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace AI_Insertion_Sort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Sort_button = new System.Windows.Forms.Button();
+            this.random_button = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.delay_timer = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pause_button = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.Salmon;
+            series1.Legend = "Legend1";
+            series1.Name = "Data1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1033, 433);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Sort_button
+            // 
+            this.Sort_button.Location = new System.Drawing.Point(535, 451);
+            this.Sort_button.Name = "Sort_button";
+            this.Sort_button.Size = new System.Drawing.Size(120, 32);
+            this.Sort_button.TabIndex = 1;
+            this.Sort_button.Text = "Sort";
+            this.Sort_button.UseVisualStyleBackColor = true;
+            this.Sort_button.Click += new System.EventHandler(this.Sort_button_Click);
+            // 
+            // random_button
+            // 
+            this.random_button.Location = new System.Drawing.Point(394, 451);
+            this.random_button.Name = "random_button";
+            this.random_button.Size = new System.Drawing.Size(114, 32);
+            this.random_button.TabIndex = 2;
+            this.random_button.Text = "Random data";
+            this.random_button.UseVisualStyleBackColor = true;
+            this.random_button.Click += new System.EventHandler(this.random_button_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "25",
+            "50",
+            "100",
+            "250",
+            "500",
+            "1000"});
+            this.comboBox1.Location = new System.Drawing.Point(83, 457);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(62, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 461);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Data number";
+            // 
+            // delay_timer
+            // 
+            this.delay_timer.Enabled = true;
+            this.delay_timer.Interval = 10;
+            this.delay_timer.Tick += new System.EventHandler(this.delay_timer_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Delay";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(233, 457);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "20";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 461);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Millisecond";
+            // 
+            // pause_button
+            // 
+            this.pause_button.Location = new System.Drawing.Point(676, 451);
+            this.pause_button.Name = "pause_button";
+            this.pause_button.Size = new System.Drawing.Size(120, 32);
+            this.pause_button.TabIndex = 8;
+            this.pause_button.Text = "Pause";
+            this.pause_button.UseVisualStyleBackColor = true;
+            this.pause_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(812, 451);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Begin: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(812, 470);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "End: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 499);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pause_button);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.random_button);
+            this.Controls.Add(this.Sort_button);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "AI Insertion Sort";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Sort_button;
+        private System.Windows.Forms.Button random_button;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer delay_timer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button pause_button;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ AI Insertion Sort/AI Insertion Sort/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AI_Insertion_Sort
+{
+    public partial class Form1 : Form
+    {
+        int[] data = new int[1001];
+        int first_data, Flag_start = 0;
+        int b = 1;
+        bool pause = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void random_button_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            this.chart1.Series["Data1"].Points.Clear();
+
+            for (int a = 1; a <= int.Parse(comboBox1.Text); a++)
+            {
+                data[a-1] = random.Next(1, int.Parse(comboBox1.Text));
+                this.chart1.Series["Data1"].Points.Add(data[a - 1]);
+            }
+        }
+
+        private void Sort_button_Click(object sender, EventArgs e)
+        {
+            b = 1;
+            
+            delay_timer.Start();
+            delay_timer.Interval = int.Parse(textBox1.Text);
+
+            label4.Text = "Begin:" + DateTime.Now.ToString("h:mm:ss tt");
+            label5.Text = "End: -";
+
+            Flag_start = 1;
+        }
+
+        private void delay_timer_Tick(object sender, EventArgs e)
+        {
+            delay_timer.Stop();
+
+            if (pause)
+            {
+                first_data = data[b];
+
+                for (int c = b - 1; c >= 0; c--)
+                {
+                    if (first_data < data[c])
+                    {
+                        data[c + 1] = data[c];
+
+                        data[c] = first_data;
+
+                        first_data = data[c];
+
+                        this.chart1.Series["Data1"].Points.Clear();
+
+                        for (int a = 0; a <= int.Parse(comboBox1.Text) - 1; a++)
+                        {
+                            this.chart1.Series["Data1"].Points.Add(data[a]);
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                b++;
+            }
+
+            if (b < int.Parse(comboBox1.Text))
+            {
+                delay_timer.Start();
+            }
+            else
+            {
+                delay_timer.Stop();
+                if (Flag_start==1)
+                {
+                    label5.Text = "End:" + DateTime.Now.ToString("h:mm:ss tt");
+                    Flag_start = 0;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pause == true) { pause = false; pause_button.Text = "countinue"; } else { pause = true; pause_button.Text = "pause"; }
+        }
+    }
+}
