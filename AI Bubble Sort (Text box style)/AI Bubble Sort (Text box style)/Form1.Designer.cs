91af4592d48094dde370742b3fb31ea073e128f6
Scuderiasy <40252740+Scuderiasy@users.noreply.github.com>
2019-09-27 07:53:51 +0700

    Add files via upload

---
+++ AI Bubble Sort (Text box style)/AI Bubble Sort (Text box style)/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace AI_Bubble_Sort__Text_box_style_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sorting_button = new System.Windows.Forms.Button();
+            this.random_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.delay_timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // sorting_button
+            // 
+            this.sorting_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.sorting_button.Location = new System.Drawing.Point(268, 141);
+            this.sorting_button.Name = "sorting_button";
+            this.sorting_button.Size = new System.Drawing.Size(134, 49);
+            this.sorting_button.TabIndex = 13;
+            this.sorting_button.Text = "Sort";
+            this.sorting_button.UseVisualStyleBackColor = true;
+            this.sorting_button.Click += new System.EventHandler(this.sorting_button_Click);
+            // 
+            // random_button
+            // 
+            this.random_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.random_button.Location = new System.Drawing.Point(12, 141);
+            this.random_button.Name = "random_button";
+            this.random_button.Size = new System.Drawing.Size(131, 49);
+            this.random_button.TabIndex = 9;
+            this.random_button.Text = "Random";
+            this.random_button.UseVisualStyleBackColor = true;
+            this.random_button.Click += new System.EventHandler(this.random_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(16, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(43, 45);
+            this.textBox1.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox2.Location = new System.Drawing.Point(65, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(43, 45);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox3.Location = new System.Drawing.Point(114, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(43, 45);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox4.Location = new System.Drawing.Point(163, 31);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(43, 45);
+            this.textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox5.Location = new System.Drawing.Point(212, 31);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(43, 45);
+            this.textBox5.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox6.Location = new System.Drawing.Point(261, 31);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(43, 45);
+            this.textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox7.Location = new System.Drawing.Point(310, 31);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(43, 45);
+            this.textBox7.TabIndex = 18;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox8.Location = new System.Drawing.Point(359, 31);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(43, 45);
+            this.textBox8.TabIndex = 18;
+            // 
+            // delay_timer
+            // 
+            this.delay_timer.Tick += new System.EventHandler(this.delay_timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 218);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sorting_button);
+            this.Controls.Add(this.random_button);
+            this.Name = "Form1";
+            this.Text = "AI Bubble Sort (Text box style)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button sorting_button;
+        private System.Windows.Forms.Button random_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Timer delay_timer;
+    }
+}
+
---
+++ AI Bubble Sort (Text box style)/AI Bubble Sort (Text box style)/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AI_Bubble_Sort__Text_box_style_
+{
+    public partial class Form1 : Form
+    {
+        double[] data = new double[10];
+        double temp_data;
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void random_button_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            Array.Clear(data, 0, 10);
+            i = 0;
+
+            for (int aa = 0; aa < 8; aa++)
+            {
+                int number = random.Next(1, 10);
+                data[aa] = number;
+            }
+
+            textBox1.Text = data[0].ToString();
+            textBox2.Text = data[1].ToString();
+            textBox3.Text = data[2].ToString();
+            textBox4.Text = data[3].ToString();
+            textBox5.Text = data[4].ToString();
+            textBox6.Text = data[5].ToString();
+            textBox7.Text = data[6].ToString();
+            textBox8.Text = data[7].ToString();
+        }
+
+        private void delay_timer_Tick(object sender, EventArgs e)
+        {
+            if (i < 8)
+            {
+                for (int j = 0; j < (8 - 1) - i; j++)
+                {
+                    if (data[j] > data[j + 1])
+                    {
+                        temp_data = data[j + 1];
+                        data[j + 1] = data[j];
+                        data[j] = temp_data;
+                    }
+                }
+
+                textBox1.Text = data[0].ToString();
+                textBox2.Text = data[1].ToString();
+                textBox3.Text = data[2].ToString();
+                textBox4.Text = data[3].ToString();
+                textBox5.Text = data[4].ToString();
+                textBox6.Text = data[5].ToString();
+                textBox7.Text = data[6].ToString();
+                textBox8.Text = data[7].ToString();
+            }
+
+            i++;
+
+            if (i >= 8)
+            {
+                delay_timer.Stop();
+            }
+        }
+
+        private void sorting_button_Click(object sender, EventArgs e)
+        {
+            delay_timer.Enabled = true;
+            delay_timer.Interval = 1000;
+            delay_timer.Start();
+        }
+    }
+}
